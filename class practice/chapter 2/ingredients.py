07771c7fefdd56713f783e333faae3d55fe683ad
Uluru92 <jorddy.castro@gmail.com>
2025-08-05 12:33:53 -0600

    introduction to OOP practice...

---
+++ class practice/chapter 2/ingredients.py
@@ -0,0 +1,8 @@
+ingredients_list = ["carrot", "pea", "squash"]
+
+ingredients_dictionary = {
+                'carrots': {'name': 'carrot', 'amount': 3},
+                'peas': {'name': 'pea', 'amount': 12}
+              }
+
+print(f"Let's take {ingredients_dictionary['carrots']['amount']} of {ingredients_dictionary['carrots']['name']} and {ingredients_dictionary['peas']['amount']} of {ingredients_dictionary['peas']['name']}.")
