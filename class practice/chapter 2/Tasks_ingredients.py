fde861cdc414c0cc14fc58070c6d9b621926e391
Uluru92 <jorddy.castro@gmail.com>
2025-08-05 18:57:00 -0600

    task ingredients chapter 2 DONE

--- class practice/chapter 2/Tasks_ingredients.py
+++ class practice/chapter 2/Tasks_ingredients.py
@@ -1,24 +1,25 @@
 class Ingredient:
     """Models an Ingredient. Currently only carrots!"""
 
-    def __init__(self,name: str,quantity: int,measure: str):
+    def __init__(self,name: str,quantity: int,measure: str, id:int):
         self.name = name
         self.quantity = quantity
         self.measure = measure
-    def print_ingredients(self):
-        print(f"Ingredient: {self.name}, add {self.quantity} {self.measure}")
+        self.id = id
+    def print_ingredient(self):
+        print(f"Ingredient {self.id}: {self.name}, add {self.quantity} {self.measure}")
 
-mint = Ingredient("mint",10,"leaves")
-alcohol = Ingredient("white rum",1.5,"ml")
-sugar = Ingredient("white sugar",2,"tea spoons")
-lime = Ingredient("lime",0.5,"unit")
-soda  = Ingredient("club soda",90,"ml")
-ice  = Ingredient("ice",4,"cubes")
+mint = Ingredient("mint",10,"leaves",1)
+alcohol = Ingredient("white rum",1.5,"ml",2)
+sugar = Ingredient("white sugar",2,"tea spoons",3)
+lime = Ingredient("lime",0.5,"unit",4)
+soda  = Ingredient("club soda",90,"ml",5)
+ice  = Ingredient("ice",4,"cubes",6)
 
 print("Recipe for a Mojito Cubano:")
-mint.print_ingredients()
-alcohol.print_ingredients()
-sugar.print_ingredients()
-lime.print_ingredients()
-soda.print_ingredients()
-ice.print_ingredients()
+mint.print_ingredient()
+alcohol.print_ingredient()
+sugar.print_ingredient()
+lime.print_ingredient()
+soda.print_ingredient()
+ice.print_ingredient()
