1a3e1efef22d1e4ade04cfdf19ae146bad087ec9
Uluru92 <jorddy.castro@gmail.com>
2025-08-12 23:03:40 -0600

    RPG game start... class Hero and its basic atributes implemented

---
+++ python-301-main/projects/rpg_game.py
@@ -0,0 +1,14 @@
+class Hero:
+    '''Main character'''
+    def __init__(self, name:str, hp_max:int, hp:int, magic_resist:int, armor:int, strenght: int, mana:int, agility:int, level:int, experience:int ):
+        self.name = name
+        self.hp_max = hp_max
+        self.hp = hp
+        self.magic_resist = magic_resist
+        self.armor = armor
+        self.mana = mana
+        self. agility = agility
+        self.level = level
+        self.experience = level
+  
+        pass
