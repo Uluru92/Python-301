b7f6ebac69b893df08901c328ee497fdeccf2fb5
Uluru92 <jorddy.castro@gmail.com>
2025-08-18 11:05:17 -0600

    03_02_inheritance_DONE

--- python-301-main/03_inheritance/03_02_inheritance_DONE.py
+++ python-301-main/03_inheritance/03_02_inheritance_DONE.py
@@ -34,16 +34,20 @@ class ActionMovie(Movie):
         self.pg = 13
         self.ticket_price = ticket_price
 
-# Lets create some RomCom movies!
+    def check_age (self, age):
+        if age < self.pg:
+            print(f"Because you are under 13, with just {age} of living in this earth, you are not allowed to see the film {self.title}.")
+        else:
+            print(f"You are {age} year old, you have no problem wacthing the film {self.title}")
 
+# Lets create some RomCom movies!
 romcom_movie_1 = RomCom(1992,
                         "Julieta y Romeo",
                         215,
                         4.5,
                         ["Vecine","Cannes","Sundance Film Festival"],
                         ["Romeo and Juliet","Charles and Mary","Amanda and Josh"],
-                        100)
-                        
+                        100)                        
 romcom_movie_2 = RomCom(2024,
                         "Sex and the City",
                         180,
@@ -51,7 +55,18 @@ romcom_movie_2 = RomCom(2024,
                         ["Berlinale","Sundance Film Festival"],
                         ["Carrie Bradshaw and Mr. Big", "Charlotte and Harry","Miranda and Steve"],
                         125)
-
 # Lets check the couples that ended together for each RomCom movie!
+print("\nLets check the couples that ended together for each RomCom movie:\n")
 romcom_movie_1.happy_ending()
 romcom_movie_2.happy_ending()
+# Lets create some Action movies!
+action_movie_1 = ActionMovie(2000,"The final battle", 124, 3,[],13,80)
+action_movie_2 = ActionMovie(2000,"Underground", 104, 5,[],18,150)
+# Lets check some ages allowed to watch a film
+print("\nLets check some ages allowed to watch a film:\n")
+age_1 = 12
+age_2 = 18
+action_movie_1.check_age(age_1)
+action_movie_1.check_age(age_2)
+action_movie_2.check_age(age_1)
+action_movie_2.check_age(age_2)
