b63aa8b91c67405b3a146aee38c1a1d61191ba58
Uluru92 <jorddy.castro@gmail.com>
2025-08-17 20:11:26 -0600

    03_01_more_food_DONE

---
+++ class practice/chapter 3/spice.py
@@ -0,0 +1,43 @@
+class Ingredient:
+
+    """Models a food item used as an ingredient."""
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
+
+    def expire(self):
+        """Expires the ingredient."""
+        print(f"whoops, these {self.name} went bad...")
+        self.name = "expired " + self.name
+
+    def __str__(self):
+        return f"{self.name} ({self.amount})"
+    
+    def __repr__(self):
+        return f"Ingredient(name={self.name}, amount={self.amount})"
+
+class Spice(Ingredient):
+    """Models a spice to flavor your food."""
+    def grind(self):
+        print(f"You have now {self.amount} of ground {self.name}.")
+    
+    def eat_half(self):
+        print(f"You have an amount of {self.amount}... you are about to eat half...")
+        self.amount = self.amount/2
+        print(f"YOU ATE HALF! Now you have an amount of {self.amount}... ")
+        
+
+p = Ingredient('peas', 12)
+print(p)  # OUTPUT: You have 12 peas.
+s = Spice('salt', 200)
+print(s)  # OUTPUT: You have 200 salt.
+
+s.expire()
+print(s)
+
+pepper = Spice("pepper", 123)
+pepper.grind()
+# p.grind() ----> # OUTPUT: AttributeError: 'Ingredient' object has no attribute 'grind'
+
+pepper.eat_half()
+p.grind() # we cannot use the method .eat_half() because this method is from the child class, not the parent class Ingredient ;]
--- python-301-main/03_inheritance/03_01_more_food_DONE.py
+++ python-301-main/03_inheritance/03_01_more_food_DONE.py
@@ -22,6 +22,30 @@ class Ingredient:
 class Spice(Ingredient):
     """Models a spice to flavor your food."""
 
+    def expire(self):
+        """Expires the ingredient item."""
+        print(f"Oh no! these {self.name} got expired...")
+        self.name = "expired " + self.name
+
     def grind(self):
         print(f"You have now {self.amount} of ground {self.name}.")
 
+    def eat_half(self):
+        print(f"You have an amount of {self.amount}... you are about to eat half...")
+        self.amount = self.amount/2
+        print(f"YOU ATE HALF! Now you have an amount of {self.amount}... ")
+        
+
+p = Ingredient('peas', 12)
+s = Spice('Spice salt', 200)
+
+p.expire()
+print(p)
+s.expire()
+print(s)
+
+pepper = Spice("Spice pepper", 123)
+pepper.grind()
+pepper.eat_half()
+
+p.grind() # we cannot use the method .eat_half() because this method is from the child class, not the parent class Ingredient ;]
