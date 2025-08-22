3263190d058c779c67f9cee1532141288ef2cdec
Uluru92 <jorddy.castro@gmail.com>
2025-08-22 00:16:23 -0600

    working on 04_03_poke_info

--- python-301-main/04_web-scraping/04_03_poke_info.py
+++ python-301-main/04_web-scraping/04_03_poke_info.py
@@ -11,4 +11,11 @@
 # `.html` page which allows you to also display the sprites of each Pokémon.
 # Check out the guides they provide: https://pokeapi-how.appspot.com/page5
 
-BASE_URL = "https://pokeapi.co/api/v2/"
+BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+
+# List of my 6 favorites pokemon:
+list_pokemons = ["bulbasaur","charmander","charmeleon","charizard","pidgeotto","pikachu"]
+
+for pokemon in list_pokemons:
+    url = f"{BASE_URL}{pokemon}"
+    print(url)
