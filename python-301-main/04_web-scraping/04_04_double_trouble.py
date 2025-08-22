6f0715dbfe67a1dc61b45ae73644d07d1af193e0
Uluru92 <jorddy.castro@gmail.com>
2025-08-22 12:47:58 -0600

    04_04_double_trouble all cat links saved in a list

--- python-301-main/04_web-scraping/04_04_double_trouble.py
+++ python-301-main/04_web-scraping/04_04_double_trouble.py
@@ -4,3 +4,20 @@
 # data that you receive from both of them.
 # E.g.: You could use the Ghibli API to find all ghosts from their films, and
 #       create an opposing team of Ghost Pokémon from the Poke API.
+
+# Solution: I will follow the recomendation but with cats -> Ghibli Cats vs My 6 Favorites Pokemons!
+
+import requests
+
+cats_url = []
+
+url_SG = "https://ghibliapi-iansedano.vercel.app/api/species"
+response = requests.get(url_SG)
+data = response.json()
+
+for specie in data['data']['species']:
+    if specie['name'] == "Cat":
+        cats_url = specie['people']
+
+for link in cats_url:
+    print(link)
