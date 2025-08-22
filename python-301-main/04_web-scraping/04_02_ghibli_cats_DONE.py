b95ff214a69add2c2d44ee1c5c294c3ada7bd13f
Uluru92 <jorddy.castro@gmail.com>
2025-08-21 23:38:39 -0600

    FORMAT

--- python-301-main/04_web-scraping/04_02_ghibli_cats_DONE.py
+++ python-301-main/04_web-scraping/04_02_ghibli_cats_DONE.py
@@ -5,11 +5,9 @@
 #
 # Your task is to use the API to find information about all the cats that
 # appear in Studio Ghibli films.
-
 # Solution:
 
 import requests
-
 BASE_URL = "https://ghibliapi-iansedano.vercel.app/api/species/603428ba-8a86-4b0b-a9f1-65df6abef3d3"
 
 page = requests.get(BASE_URL)
@@ -30,4 +28,3 @@ for index,link in enumerate(links,start = 1):
     print(f'Age: {age}')
     print(f'Eye color: {eye_color}')
     print(f'Hair color: {hair_color}')
-
