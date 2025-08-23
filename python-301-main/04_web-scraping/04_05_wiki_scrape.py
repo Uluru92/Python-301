1d343d586bbb5fd77b258faac595e4350e86d736
Uluru92 <jorddy.castro@gmail.com>
2025-08-22 19:19:48 -0600

    exercise 04_05 working with text inside each link

--- python-301-main/04_web-scraping/04_05_wiki_scrape.py
+++ python-301-main/04_web-scraping/04_05_wiki_scrape.py
@@ -5,4 +5,35 @@
 # extract the text content from that article, and save it to a local text file.
 # BONUS TASK: Use RegExp to find all numbers in the text.
 
+# Solution:
+
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
 URL = "https://en.wikipedia.org/wiki/Web_scraping"
+request = requests.get(URL)
+soup = BeautifulSoup(request.text,"html.parser")
+
+content_div = soup.find("div", class_="mw-content-container")
+
+links = soup.find_all("a",href=True)
+
+links = []
+if content_div:
+    for link in content_div.find_all("a", href=True):
+        href = link["href"]
+        if href.startswith("http://") or href.startswith("https://"):
+            full_url = href
+
+        else:
+            full_url = urljoin(URL, href)  # make url absolute
+        
+        if not full_url.startswith("#"):  # skip fragments that stays on same page
+            links.append(full_url)
+
+for l in links:
+    sub_request = requests.get(l)
+    sub_soup = BeautifulSoup(sub_request.text,"html.parser")
+    sub_title = sub_soup.find("h1", class_="firstHeading mw-first-heading")
+    sub_content_div = sub_soup.find("div", class_="mw-content-ltr mw-parser-output")
