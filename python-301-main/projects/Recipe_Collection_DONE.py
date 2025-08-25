f192322b6960168b741f37606772dcf93c6b79ae
Uluru92 <jorddy.castro@gmail.com>
2025-08-25 16:16:25 -0600

    Recipe_Collection_DONE

--- python-301-main/projects/Recipe_Collection_DONE.py
+++ python-301-main/projects/Recipe_Collection_DONE.py
@@ -91,14 +91,50 @@ if __name__ == "__main__":
     recipe_links = []
 
     for tag in tags_a:
-        recipe_links.append(tag["href"])
+        recipe_links.append(f"https://codingnomads.github.io/recipes/{tag['href']}")
     
-    # Lets create an html for each link, to analize them later!
-    for index,link in enumerate(recipe_links):
-        print(f'{index} - {link}')
+    # Lets ask the user to provide 3 ingredients so we can search recipes with them in each link
+    print("Ok now is your turn to participate user! Give me 3 ingredients to built a recipe!")
+    Ingredient_by_user_1 = input("Ingrediente 1: ").lower()
+    Ingredient_by_user_2 = input("Ingrediente 2: ").lower()
+    Ingredient_by_user_3 = input("Ingrediente 3: ").lower()
+    ingredients = [Ingredient_by_user_1, Ingredient_by_user_2, Ingredient_by_user_3]
 
-    def main_code():
-        print("Ok now is your turn to participate user! Give me 3 ingredients to built a recipe!")
-        Ingredient_by_user_1 = input("Ingrediente 1: ")
-        Ingredient_by_user_2 = input("Ingrediente 2: ")
-        Ingredient_by_user_3 = input("Ingrediente 3: ")
+    # Lets analize link by link searching for ingredients provided by user!
+    found = False
+    for index,link in enumerate(recipe_links):
+
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text, "html.parser")
+        tags_p = soup.find_all("p")
+        all_text = ""
+        for p in tags_p:
+            classes = p.get("class", [])
+            if "subtitle" in classes and "author" in classes:
+                continue
+            all_text += p.get_text().lower() + " "
+        
+        author_tag = soup.find("p", class_="subtitle is-3 author")
+        if author_tag:
+            author = author_tag.get_text().strip()
+            if author.lower().startswith("by "):
+                author = author[3:].strip()  # elimina "by " al inicio
+            # También eliminamos de all_text si empieza con "by <author>"
+            if all_text.lower().startswith(f"by {author.lower()}"):
+                all_text = all_text[len(f"by {author.lower()}"):].strip()
+        else:
+            author = "Author not found"
+
+        for ingredient in ingredients:
+            if ingredient in all_text:
+                print(f"✅ Found '{ingredient}' in the link {link}:")
+                title = soup.find("title").get_text()
+                clean_title = title.split(" by ")[0]
+                print(f"Title: {clean_title}")
+                print("Author:", author)
+                print("Text preview:\n", all_text)
+                print(" ")
+                found = True
+    
+    if not found:
+        print("❌ No ingredients found") 
