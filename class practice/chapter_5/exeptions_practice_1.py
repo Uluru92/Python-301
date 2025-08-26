98705e7ee14177f882a55ba62ba91c27f9f13a58
Uluru92 <jorddy.castro@gmail.com>
2025-08-26 11:15:28 -0600

    practice chapter 5

---
+++ class practice/chapter_5/exeptions_practice_1.py
@@ -0,0 +1,13 @@
+class AgeBelowZeroError(Exception): pass
+
+while True:
+    try:
+        age = int(input("Age: "))
+        if age < 0:
+            raise AgeBelowZeroError()
+    except ValueError:
+        print("Please enter a numeric value.")
+    except AgeBelowZeroError:
+        print("Looks like you've not been born yet.")
+    else:
+        print(f"Congratulations, you're {age} years old!")
