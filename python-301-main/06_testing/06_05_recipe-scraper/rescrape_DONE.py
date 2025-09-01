af32046ff68f18a6aaae5b1bec485f490e54fd1d
Uluru92 <jorddy.castro@gmail.com>
2025-08-31 23:52:43 -0600

    rescrape chapter 6 done

---
+++ python-301-main/06_testing/06_05_recipe-scraper/rescrape_DONE.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://codingnomads.github.io/recipes/"
+
+def get_page_content(url):
+    """Gets the response from a HTTP call to the URL."""
+    page = requests.get(url)
+    return page
+
+def get_html_content(url):
+    response = get_page_content(url)
+    html = response.text
+    return html
+
+def get_bs4_object(url):
+    html = get_html_content(url)
+    soup = BeautifulSoup(html, "html.parser")
+    return soup
+
+def get_all_links_from_index(url):
+    index_soup = get_bs4_object(url)
+    links = [a["href"] for a in index_soup.find_all("a", href=True)]
+    return links
+
+def get_author_from_index(url):
+    links = get_all_links_from_index(url)
+
+    authors = []
+    for link in links:
+        sub_url = url + link
+        sub_soup = get_bs4_object(sub_url)
+        author_tag = sub_soup.find("p", class_="subtitle is-3 author")
+        if author_tag:
+            author = author_tag.get_text(strip=True).replace("by ", "", 1)
+            authors.append(author)
+
+    return authors
+
+def get_titles_from_index(url):
+    links = get_all_links_from_index(url)
+
+    titles = []
+    for link in links:
+        sub_url = url + link
+        sub_soup = get_bs4_object(sub_url)
+
+        title_tag = sub_soup.find("h1", class_="title is-2")
+        if title_tag:
+            title = title_tag.get_text(strip=True)
+            titles.append(title)
+
+    return titles
+
+def get_recipes_from_index(url):
+    links = get_all_links_from_index(url)
+
+    recipes = []
+    for link in links:
+        sub_url = url + link
+        sub_soup = get_bs4_object(sub_url)
+        recipe_text = sub_soup.find("div", class_="md")
+        if recipe_text:
+            recipe = recipe_text.text.strip()
+            recipes.append(recipe)
+    return recipes
+
+if __name__ == "__main__":
+    titles = get_titles_from_index(BASE_URL)
+    authors = get_author_from_index(BASE_URL)
+    recipes = get_recipes_from_index(BASE_URL)
+
+    for title, author, recipe in zip(titles, authors, recipes):
+        print(f"Title: {title}\nAuthor: {author}\nRecipe: {recipe}\n")
+        print(f"-"*50)
--- python-301-main/06_testing/06_05_recipe-scraper/test_rescrape.py
+++ python-301-main/06_testing/06_05_recipe-scraper/test_rescrape.py
@@ -1,4 +1,69 @@
-# Write a unittest test suite to test the rescrape module
-
 import unittest
-import rescrape
+from rescrape_DONE import *
+
+class TestRescrape(unittest.TestCase):
+
+    # setUp variables for all tests...
+    def setUp(self):
+        self.BASE_URL = "https://codingnomads.github.io/recipes/"
+        self.url = f"{self.BASE_URL}recipes/11-making-my-own-baguet.html"
+
+    # requests can establish a connection and receive a valid response
+    def test_get_valid_html_response(self):
+        self.assertEqual(get_page_content(BASE_URL).status_code, 200)
+
+    # the response contains HTML code
+    def test_get_html_content_returns_html_string(self):
+        page = get_html_content(BASE_URL)
+        self.assertIn("<!DOCTYPE html>", page)
+
+    # the HTML can be successfully converted to a Beautiful Soup object
+    def test_get_bs4_content_returns_bs4_object(self):
+        page = get_bs4_object(BASE_URL)
+        self.assertIsInstance(page, BeautifulSoup)
+
+    # can identify all links from the index page
+    def test_identify_all_links_from_index_page(self):
+        links = get_all_links_from_index(BASE_URL)
+        self.assertIsInstance(links,list) # Verify we get a list
+        self.assertGreater(len(links),0) # Verify the list is not empty
+        self.assertIn("recipes/19-wasn-t-sure-how-to-p.html",links) # Verify an existing link
+
+    # can identify the author of a recipe
+    def test_can_identify_author(self):
+
+        authors = get_author_from_index(BASE_URL)
+
+        self.assertIsInstance(authors, list) # Verify we get a list
+        self.assertGreater(len(authors), 0) # Verify the list is not empty
+
+        for author in authors:
+            self.assertIsInstance(author, str) # Verify we get a string
+            self.assertFalse(author.lower().startswith("by "))  # Verify the prefix "by " was deleted
+            self.assertGreater(len(author.strip()), 0)  # Verify the list is not empty
+            self.assertIn("swilliams2207",authors) # Verify an existing author
+
+    # can get titles
+    def test_can_identify_titles(self):
+        titles = get_titles_from_index(BASE_URL)
+
+        self.assertIsInstance(titles, list)
+        self.assertGreater(len(titles), 0)
+
+        for title in titles:
+            self.assertIsInstance(title, str)
+            self.assertGreater(len(title.strip()), 0)
+
+        self.assertIn("Garlic Butter Steak and Potatoes Skillet", titles)  # ejemplo de título esperado
+
+    # can get the main recipe text
+    def test_get_main_recipe_text(self):
+        recipes = get_recipes_from_index(BASE_URL)
+
+        self.assertIsInstance(recipes, list) # Verify we get a list
+        self.assertGreater(len(recipes), 0) # Verify the list is not empty
+        for recipe in recipes:
+            self.assertIsInstance(recipe, str)# Verify we get a string
+    
+if __name__ == "__main__":
+    unittest.main()
