d783c6a0a2c03023bdb88f3e70dde71a8e8f5aa4
Uluru92 <jorddy.castro@gmail.com>
2025-09-02 14:14:27 -0600

    06_05_revisit_testing advancing well...

--- python-301-main/06_testing/06_05_revisit_testing.py
+++ python-301-main/06_testing/06_05_revisit_testing.py
@@ -23,8 +23,31 @@
 import unittest
 import requests
 import json
+import os
 
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+list_pokemons = ["bulbasaur","charmander","charmeleon","charizard","pidgeotto","pikachu"]
+
+def create_folder_to_keep_json_files(path):
+    os.makedirs('files_exercise_06_05', exist_ok=True)
+    return True
+
+def get_response(url):
+    response = requests.get(url)
+    return response
+
+def get_data_json_from_response(url):
+    response = get_response(url)
+    data = response.json()
+    return data
+
+create_folder_to_keep_json_files('files_exercise_06_05')
+
+for index,pokemon in enumerate(list_pokemons):
+    url = f"{BASE_URL}{pokemon}"
+    data = get_data_json_from_response(url)
+    with open(rf"files_exercise_06_05\pokemon_{index+1}.json", "w", encoding="utf-8") as fout:
+        json.dump(data, fout, indent=4, ensure_ascii=False)
 
 class TestAssembleTeam(unittest.TestCase):
     # setUp variables for all tests...
@@ -32,3 +55,19 @@ class TestAssembleTeam(unittest.TestCase):
         self.BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
         self.list_pokemons = ["bulbasaur","charmander","charmeleon","charizard","pidgeotto","pikachu"]
     
+    # Check if my list have all 6 pokemons
+    def test_my_len_list(self):
+        self.assertEqual(len(self.list_pokemons),6)
+
+    # Check if the response is ok (200)
+    def test_get_response_200(self):
+        self.assertEqual(get_response(BASE_URL).status_code,200)
+
+    # Check if the response was transforme to a .json data
+    def test_get_data_json_from_response(self):
+        data = get_data_json_from_response(BASE_URL)
+        self.assertIsInstance(data, (dict, list))
+
+if __name__ == "__main__":
+    unittest.main()
+        
