058297d8fa5e4af306bef41c720d2165c6d2217a
Uluru92 <jorddy.castro@gmail.com>
2025-09-06 00:04:47 -0600

    using fields and asdict

--- class practice/chapter_9/example_namedtuple.py
+++ class practice/chapter_9/example_namedtuple.py
@@ -11,5 +11,16 @@ print(null_island[2])  # "0"
 
 # Using dot notation
 print(null_island.name)  # "Null Island"
+
+# Using ._fields
 print(null_island._fields)  # "('name', 'lat', 'lon')"
+print(null_island._fields[0])  # "('name', 'lat', 'lon')"
+print(null_island._fields[1])  # "'lat'
+print(null_island._fields[2])  # "'lon'
+
+# Using ._asdict
 print(null_island._asdict())  # "{'name': 'Null Island', 'lat': 0, 'lon': 0}"
+print(null_island._asdict()["name"]) # "Null Island"
+print(null_island._asdict()["lat"])  # "0"
+print(null_island._asdict()["lon"])  # "0"
+
